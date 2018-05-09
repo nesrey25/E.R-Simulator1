567a4d694893e6d6f12cec88db4f4babf4b0b6aa
nesrey25 <35348606+nesrey25@users.noreply.github.com>
2018-05-09 13:31:27 -0700

    Add files via upload

--- Patient.cpp
+++ Patient.cpp
@@ -7,3 +7,34 @@
 //
 
 #include "Patient.hpp"
+#include "Menu.hpp"
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+
+int read_int(const std::string &prompt, int low, int high)
+{
+    if (low >= high) // invalid range
+        throw std::invalid_argument("invalid range specified");
+    
+    std::cin.exceptions(std::ios_base::failbit);
+    int num = 0;
+    while (true) {
+        try {
+            while (true) {
+                std::cout << prompt;
+                std::cin >> num;
+                if (num >= low && num <= high) { // within the specified range
+                    std::cout << std::endl;
+                    return num;
+                }
+            }
+        }
+        catch (std::ios_base::failure) {
+            std::cout << "Bad numeric string -- try again\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<int>::max(), '\n');
+        }
+    }
+}
